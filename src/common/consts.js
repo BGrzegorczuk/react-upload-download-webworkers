1ea9ae00b1334cdd756c2db1fcc04c0a92800cf8
Bartosz Grzegorczuk <bartosz.grzegorczuk@gmail.com>
2017-12-11 16:06:43 +0100

    action types and consts

--- src/common/consts.js
+++ src/common/consts.js
@@ -4,6 +4,7 @@ export const MSG_TYPES = {
     ERROR: -1,
     INIT: 0,
     PROCESS: 1,
+
     UPLOAD_INIT: 2,
     UPLOAD_START: 3,
     UPLOAD_PROGRESS: 4,
@@ -15,14 +16,14 @@ export const MSG_TYPES = {
     LOG: 8
 };
 
-export const WORKER_STATUSES = {
+export const WORKER_STATES = {
     IDLE: 1,
     PROCESSING: 2,
     BROKEN: 3
 };
 
 // TODO: reconsider
-export const TASK_STATUSES = {
+export const TASK_STATES = {
     NEW: 1,
     ASSIGNED: 2,
     PROCESSING: 3,
